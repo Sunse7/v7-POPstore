ecc89971c6a56994c4d72aa9c8e67be455fd4411
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-02-16 09:58:36 +0100

    It works!

--- main.js
+++ main.js
@@ -1,25 +1,64 @@
 const API_URL = 'products.json';
 const cardContainer = document.querySelector('.card-container');
+const shoppingCartContainer = document.querySelector('.cart-items');
+let popcornsArray = []
+
+getLocalStorage();
+fetchData();
+
+function getLocalStorage() {
+    let currentPopcorn = JSON.parse(localStorage.getItem('popcornsInfo'));
+    if (currentPopcorn) {
+        currentPopcorn.forEach(element => {
+            popcornsArray.push(element);
+            console.log(popcornsArray, 'still arr?');
+        });
+    }
+}
 
 async function fetchData() {
     let data = await fetch(API_URL);
     response = await data.json();
-    console.log(response);
     
-    renderToUI(response);
+    if (cardContainer === null) {
+        renderCardsToUI(popcornsArray);
+    } else {
+        renderCardsToUI(response);
+    }
 }
 
-fetchData();
-
-function renderToUI(popcorns) {
+function renderCardsToUI(popcorns) {
     popcorns.forEach(popcorn => {
         let newCard = document.createElement('article');
+        let buyButton = document.createElement('button');
+        buyButton.classList.add('buy-btn');
+        buyButton.innerHTML = 'Buy!';
         newCard.classList.add('card');
         newCard.innerHTML = `
             <img class="card__image" src="img/${popcorn.img}">
             <h3 class="card__name">${popcorn.name}</h3>
             <p class="card__price">${popcorn.pricePerHekto} kr/hg</p>
         `;
-        cardContainer.appendChild(newCard);
+
+        if (shoppingCartContainer) {            
+            shoppingCartContainer.appendChild(newCard);
+        } else {
+            cardContainer.appendChild(newCard);
+            newCard.appendChild(buyButton);
+
+            buyButton.addEventListener('click', () => {   
+                popcornsArray.push(popcorn);             
+                console.log(popcornsArray, 'arr');
+            });
+            cartRedirect(popcornsArray);
+        }
     });
+}
+
+function cartRedirect(array) {
+    let cartButton = document.querySelector('.cart-btn');
+    cartButton.addEventListener('click', () => {
+        localStorage.setItem('popcornsInfo', JSON.stringify(array));
+        window.location.href = 'cart.html';
+    })
 }
